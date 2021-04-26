4c2646a185949057c5f69a5b3a5c8e86105bf1d7
MrPiedPiper <35707072+MrPiedPiper@users.noreply.github.com>
2021-04-26 01:15:51 -0700

    Add boss animations for hits and basic destruction animation

--- Bosses/Boss1/Boss1.gd
+++ Bosses/Boss1/Boss1.gd
@@ -22,4 +22,9 @@ func enable_barrier():
 
 func ship_destroyed():
 	.ship_destroyed()
-	print("DESTRUCTION")
+	$AnimationPlayer.play("Death")
+	$CPUParticles2D.emitting = true
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Death":
+		queue_free()
